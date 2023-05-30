8b5418cb6062ddb5c2f7f16eff6e4a18a9770d7e
allex <william.al1379@gmail.com>
2023-05-30 11:09:44 -0600

    Use vars to reference flow better

--- cypress/e2e/assigment2/login.cy.js
+++ cypress/e2e/assigment2/login.cy.js
@@ -1,6 +1,12 @@
+import { transaction } from "../../page-objects/pages/transaction";
+
 describe('Real Wordl app tests', () => {
+    
     // get uuid random string
-    const accountName = `Bank of America ${Cypress._.random(0, 1e6)}`
+    const accountName = `Bank of America ${Cypress._.random(0, 1e6)}`;
+    const accountNumber = '111111111';
+    const routingNumber = '111111111';
+
     beforeEach(()=> {
         cy.visit('/');
         cy.get('#username').clear().type('Katharina_Bernier');
@@ -13,8 +19,8 @@ describe('Real Wordl app tests', () => {
         cy.get('.MuiTypography-root').contains('Bank Accounts').click();
         cy.get('a[data-test="bankaccount-new"] span').contains('Create').click({force: true});
         cy.get('#bankaccount-bankName-input').clear().type(accountName);
-        cy.get('#bankaccount-routingNumber-input').clear().type('111111111');
-        cy.get('#bankaccount-accountNumber-input').clear().type('111111111');
+        cy.get('#bankaccount-routingNumber-input').clear().type(routingNumber);
+        cy.get('#bankaccount-accountNumber-input').clear().type(accountNumber);
         cy.get(".MuiButton-label").contains('Save').click();
         cy.get('ul[data-test="bankaccount-list"] > li').contains(accountName).should('be.visible');
     });
