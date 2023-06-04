ba0fda8f1f22ee0d616ad1705793f64767ea22e5
allex <william.al1379@gmail.com>
2023-06-04 10:38:04 -0600

    try other fix

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,5 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl: "http://localhost:3000",
-    chromeWebSecurity: false,
-    chromeArgs: ['--disable-gpu'],
   },
 });
