13b9a5d641c836b51b51768832752de858995d09
allex <william.al1379@gmail.com>
2023-06-05 14:22:15 -0600

    Fix

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    supportFile: false,
+    fixturesFolder: false,
     baseUrl: "http://localhost:3000",
   },
 });
